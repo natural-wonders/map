c3ec29f37b0b4209f6139c44a83d0d4c988c8546
sam <sam.woodcock@protonmail.com>
2023-08-20 20:14:14 +0100

    fix: vite devserver response headers

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,28 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		{
+			name: 'configure-response-headers',
+			configureServer: (server) => {
+				server.middlewares.use((_req, res, next) => {
+					res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+					res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+					next();
+				});
+			}
+		}
+	],
 	server: {
 		host: true,
-		port: 3001,
-		headers: {
-			'Cross-Origin-Opener-Policy': 'same-origin',
-			'Cross-Origin-Embedder-Policy': 'require-corp'
-		}
+		port: 3001
 	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
 	optimizeDeps: {
-		exclude: ['@sqlite.org/sqlite-wasm']
+		// exclude: ['@sqlite.org/sqlite-wasm']
+		exclude: ['sqlocal']
 	}
 });
