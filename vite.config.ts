ac89049c9d1d793b32647d19324aa9beade2f410
sam <sam.woodcock@protonmail.com>
2023-08-20 17:29:51 +0100

    build: add headers for OPFS on vite devserver

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,16 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		host: true,
-		port: 3001
+		port: 3001,
+		headers: {
+			'Cross-Origin-Opener-Policy': 'same-origin',
+			'Cross-Origin-Embedder-Policy': 'require-corp'
+		}
 	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	optimizeDeps: {
+		exclude: ['@sqlite.org/sqlite-wasm']
 	}
 });
