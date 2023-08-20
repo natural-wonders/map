f7d9c6d8ae0b48a397093c90ea5cc1e8042686a1
sam <sam.woodcock@protonmail.com>
2023-08-20 11:49:26 +0100

    feat: init app, sveltekit, skeleton, tailwind

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	plugins: [sveltekit()],
+	server: {
+		host: true,
+		port: 3001,
+	},
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
