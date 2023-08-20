744c6615f453856b37800cab68b6694eabca30b7
sam <sam.woodcock@protonmail.com>
2023-08-21 00:43:40 +0100

    feat: display natural wonders in table

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
 	import '../app.postcss';
 	import { AppShell } from '@skeletonlabs/skeleton';
+	import { AppBar } from '@skeletonlabs/skeleton';
 	import { onMount } from 'svelte';
-	import { SQLocal } from 'sqlocal';
 
 	import { opfsFileExists, writeBinaryToOPFS } from '$lib/fs/opfs';
 	import { downloadFile } from '$lib/fs/fetch';
 
 	onMount(async () => {
-		const dbName = 'nwm.sqlite3';
+		const dbName = import.meta.env.VITE_DB_NAME;
 
 		// Get OPFS filesystem, check DB exists
 		const opfsRoot = await navigator.storage.getDirectory();
@@ -22,22 +22,12 @@
 			const existingDb = await downloadFile(`./${dbName}`);
 			await writeBinaryToOPFS(opfsRoot, dbName, existingDb);
 		}
-
-		// Connect to DB
-		const { sql } = new SQLocal(dbName);
-		const query = await sql`SELECT * FROM naturalwonders;`;
-		console.log(query);
 	});
 </script>
 
-<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
-	<svelte:fragment slot="sidebarLeft">
-		<nav class="list-nav">
-			<ul>
-				<li><a href="/">Home</a></li>
-				<li><a href="/about">About</a></li>
-			</ul>
-		</nav>
+<AppShell>
+	<svelte:fragment slot="pageHeader">
+		<AppBar class="bg-primary-500">Natural Wonders</AppBar>
 	</svelte:fragment>
 	<slot />
 </AppShell>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,35 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { SQLocal } from 'sqlocal';
+
+	import { Table, tableMapperValues } from '@skeletonlabs/skeleton';
+
+	// let mapWonders: Array<Record<string, any>> = []
+	let allWonders: Array<Record<string, any>> = [];
+	$: nwmTable = {
+		head: ['Name', 'Tier', 'Type', 'Country'],
+		// The data visibly shown in your table body UI.
+		body: tableMapperValues(allWonders, ['name', 'tier', 'type', 'country']),
+		// Optional: The data returned when interactive is enabled and a row is clicked.
+		meta: tableMapperValues(allWonders, ['lat', 'lon', 'elev']),
+		foot: ['Total', '', `<code class="${allWonders.length}">5</code>`]
+	};
+
+	onMount(async () => {
+		// Connect to DB
+		const { sql } = new SQLocal(import.meta.env.VITE_DB_NAME);
+		// mapWonders = await sql`
+		// 	SELECT name, type, json_array(lon, lat)
+		// 	AS coord FROM naturalwonders;
+		// `;
+		allWonders = await sql`SELECT * FROM naturalwonders;`;
+	});
+</script>
+
 <div class="card p-4">
-	<ul>
-		{#each ['1', '2', '3'] as pos, index}
-			<li>
-				<div class="placeholder-circle w-16" />
-			</li>
+	<!-- <ul>
+		{#each allWonders as wonder, index}
 		{/each}
-	</ul>
+	</ul> -->
+	<Table source={nwmTable} />
 </div>
