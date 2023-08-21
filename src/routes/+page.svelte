c7ebb0ae6862deaa7b08fc712908a237d529e48a
sam <sam.woodcock@protonmail.com>
2023-08-21 04:41:52 +0100

    feat: start gleo map for loading wonders

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,22 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { SQLocal } from 'sqlocal';
 
-	import { Table, tableMapperValues } from '@skeletonlabs/skeleton';
+	import GleoMap from 'gleo/src/CartesianMap.mjs';
+	import Geometry from 'gleo/src/geometry/Geometry.mjs';
+	import LngLat from 'gleo/src/geometry/LngLat.mjs';
 
-	// let mapWonders: Array<Record<string, any>> = []
-	let allWonders: Array<Record<string, any>> = [];
-	$: nwmTable = {
-		head: ['Name', 'Tier', 'Type', 'Country'],
-		// The data visibly shown in your table body UI.
-		body: tableMapperValues(allWonders, ['name', 'tier', 'type', 'country']),
-		// Optional: The data returned when interactive is enabled and a row is clicked.
-		meta: tableMapperValues(allWonders, ['lat', 'lon', 'elev']),
-		foot: ['Total', '', `<code class="${allWonders.length}">5</code>`]
-	};
+	import { getMapWonders } from '$lib/db/get';
+
+	let wonders: Array<Record<string, any>> = [];
 
 	onMount(async () => {
-		// Connect to DB
-		const { sql } = new SQLocal(import.meta.env.VITE_DB_NAME);
-		// mapWonders = await sql`
-		// 	SELECT name, type, json_array(lon, lat)
-		// 	AS coord FROM naturalwonders;
-		// `;
-		allWonders = await sql`SELECT * FROM naturalwonders;`;
+		wonders = await getMapWonders();
+
+		// Gleo Map
+		const map = (window.map = new GleoMap('gleomap', {}));
+		map.center = [0, 0];
+		map.span = 1.3;
 	});
 </script>
 
-<div class="card p-4">
-	<!-- <ul>
-		{#each allWonders as wonder, index}
-		{/each}
-	</ul> -->
-	<Table source={nwmTable} />
-</div>
+<div id="gleomap" style="width: calc(100vw - 16px); height: 80vh; display: inline-block"></div>
